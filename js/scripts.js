85d0edafa9e687fc7396ddfdb2acda61800269b9
Santiago Higuita <santiagohiguitaardila@alumno.ieselrincon.es>
2026-01-29 09:39:21 +0000

    Fase 2: Finalizado primera version de barra de navegacion responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 // TODO: Implementar el interruptor de Modo Oscuro/Claro
 // document.documentElement.classList.toggle('dark');
 
-const NAV_BUTTON = document.querySelector('.nav-hamburger');
-const NAV_MENU = document.querySelector('.nav-menu');
+const NAV_BUTTON = document.querySelector('.nav-hamburger'); // Boton menu
+const NAV_MENU = document.querySelector('.nav-menu'); // Menu de navegacion
 
+// Hacer click en el boton esconde/muestra el menu
 NAV_BUTTON.addEventListener('click', () => {
   NAV_MENU.classList.toggle('active');
+  e.stopPropagation(); // Prevent event from bubbling to document
+});
+
+// Tocar fuera del menu oculta el menu
+document.addEventListener('click', (e) => {
+  const isClickInsideMenu = NAV_MENU.contains(e.target);
+  const isClickOnButton = NAV_BUTTON.contains(e.target);
+
+  if (!isClickInsideMenu && !isClickOnButton && NAV_MENU.classList.contains('active')) {
+    NAV_MENU.classList.remove('active');
+  }
+});
+
+// Seleccionar una seccion del menu lo oculta
+const NAV_LINKS = document.querySelectorAll('.nav-elems a');
+NAV_LINKS.forEach(link => {
+  link.addEventListener('click', () => {
+    if (NAV_MENU.classList.contains('active')) {
+      NAV_MENU.classList.remove('active');
+    }
+  });
 });
