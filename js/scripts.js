c727e4a4a304451e0e039409b9815c6ded950329
Santiago Higuita <santiagohiguitaardila@alumno.ieselrincon.es>
2026-01-29 00:10:17 +0000

    Fase 2: Continuacion de la implementacion responsive de la barra de navegacion

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 // TODO: Implementar el interruptor de Modo Oscuro/Claro
 // document.documentElement.classList.toggle('dark');
+
+const NAV_BUTTON = document.querySelector('.nav-hamburger');
+const NAV_MENU = document.querySelector('.nav-menu');
+
+NAV_BUTTON.addEventListener('click', () => {
+  NAV_MENU.classList.toggle('active');
+});
